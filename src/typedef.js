ea7af0ad8ea02508f68e6547d8801e1a76b92d3e
Aleksey Tsvetkov <acvetkov@yandex-team.ru>
2015-12-02 14:22:54 +0300

    add typedef

---
+++ src/typedef.js
@@ -0,0 +1,59 @@
+/**
+ * @author acvetkov@yandex-team.ru
+ * @overview
+ */
+
+/**
+ * @typedef Object ChromeStoreItem
+ *
+ * @property {String} kind
+ * @property {String} id
+ * @property {String} publicKey
+ * @property {String} uploadState
+ * @property {Array<String>} itemError
+ */
+
+/**
+ * @typedef Object ChromeStorePublishInfo
+ *
+ * @property {String} kind
+ * @property {String} item_id
+ * @property {Array<String>} status
+ * @property {Array<String>} statusDetail
+ */
+
+/**
+ * @typedef Object TokenData
+ *
+ * @property {String} token
+ * @property {String} refreshToken
+ */
+
+/**
+ * @typedef {Object} IStorage
+ * @property {StoreCreate} create
+ * @property {StoreGet} get
+ * @property {StoreSet} set
+ *
+ */
+
+/**
+ * @typedef Function StoreCreate
+ * @property Function create
+ * @returns {Promise}
+ */
+
+/**
+ * @typedef Function StoreGet
+ * @property Function code
+ * @param {String} code
+ * @param {String} token
+ * @param {String} refreshToken
+ * @returns {Promise}
+ */
+
+/**
+ * @typedef Function StoreSet
+ * @property Function code
+ * @returns {Promise<TokenData>}
+ */
