f37c86b12456f5c7d5c5a65a9de3bdb5095cc5ba
Aleksey Tsvetkov <acvetkov@yandex-team.ru>
2015-12-02 14:20:36 +0300

    Append urls config

---
+++ src/config/index.js
@@ -0,0 +1,11 @@
+/**
+ * @author acvetkov@yandex-team.ru
+ * @overview
+ */
+
+export default {
+    API_GET_URL: 'https://www.googleapis.com/chromewebstore/v1.1/items/itemId/publish',
+    API_UPLOAD_URL: 'https://www.googleapis.com/upload/chromewebstore/v1.1/items/',
+    API_PUBLISH_URL: 'https://www.googleapis.com/chromewebstore/v1.1/items/{itemId}/publish',
+    ACCOUNTS_URL: 'https://accounts.google.com/o/oauth2/token'
+};
