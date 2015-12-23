8be363f3dc90cc96304cca904bd1887319ad70be
Aleksey Tsvetkov <acvetkov@yandex-team.ru>
2015-12-23 17:03:32 +0300

    Fix get url

--- src/config/index.js
+++ src/config/index.js
@@ -4,7 +4,7 @@
  */
 
 export default {
-    API_GET_URL: 'https://www.googleapis.com/chromewebstore/v1.1/items/itemId/publish',
+    API_GET_URL: 'https://www.googleapis.com/chromewebstore/v1.1/items/{itemId}?projection=draft',
     API_UPLOAD_URL: 'https://www.googleapis.com/upload/chromewebstore/v1.1/items/',
     API_PUBLISH_URL: 'https://www.googleapis.com/chromewebstore/v1.1/items/{itemId}/publish',
     ACCOUNTS_URL: 'https://accounts.google.com/o/oauth2/token'
