d6cd54ba4c8d318a101809862a6742a1c890b26b
Aleksey Tsvetkov <acvetkov@yandex-team.ru>
2015-12-24 11:37:11 +0300

    Fix deps

--- test/setup.js
+++ test/setup.js
@@ -5,7 +5,6 @@
 
 var chai = require('chai');
 
-global.chrome = require('sinon-chrome');
 global.assert = chai.assert;
 global.sinon = require('sinon');
 global.sinon.assert.expose(global.assert, {prefix: ''});
